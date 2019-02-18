7725eb5ab67b420f16c87c1994eb75bb82245c55
Kate Adair <adairkate@gmail.com>
2019-02-18 12:36:32 +0800

    Add babel-preset-env to js rules

--- build/rules/javascript.js
+++ build/rules/javascript.js
@@ -1,4 +1,17 @@
+const settings = require('../webpack.settings.js');
+
 module.exports = {
   test: /\.js$/,
-  loader: ['babel-loader', 'eslint-loader']
+  use: [
+    {
+      loader: 'babel-loader',
+      options: {
+        presets: ['babel-preset-env']
+      },
+    },
+    {
+      loader: 'eslint-loader'
+    },
+  ],
+  exclude: settings.paths.external,
 }
